6bcb95955a839b252c0dbfb7babf120dfe061734
Bryan Castleman <castlemaninc@gmail.com>
2017-06-08 22:06:10 -0700

    added add note button

--- public/app.js
+++ public/app.js
@@ -1,12 +1,14 @@
+
+var x = "edit";
 // Grab the articles as a json
 $.getJSON("/api", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].date + "<br />" + "<strong>" + data[i].title + "</strong>" + "<br />" + data[i].summary + "</p>");
+    $("#articles").append("<div class='panel panel-default clearfix data-id='" + data[i]._id + "'>" + "<img src='" + data[i].imglink + "'>" + "<br /><br />" + data[i].date + "<br />" + "<strong>" + data[i].title + "</strong>" + "<br />" + data[i].summary + "<button type='button' class='btn btn-info pull-right'>" + "Add Note" + "</button>" + "</div>");
     
 
-
+				
 
 
 
